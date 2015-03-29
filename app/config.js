8d7d43d715cadae1168b8496ce0bb77191350ceb
SungMin <4sungmin@gmail.com>
2015-03-29 12:40:33 -0700

    Set up MongoDB to connect to MongoLab on Azure account.

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@
 var mongoose = require('mongoose');
 var path = require('path');
 
-mongoose.connect('mongodb://localhost/test');
+mongoose.connect('mongodb://MongoLab-3:a1.H1_Bd7k6iTj4HpxZuokOQdem17cnVPAhJ5mccmuA-@ds031108.mongolab.com:31108/MongoLab-3');
 
 var db = mongoose.connection;
 
